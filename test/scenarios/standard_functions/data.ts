f763a671424de9fe6eac4fc8bea49a713ccddfcd
Dilip Kola <kdilipkola@gmail.com>
2024-06-14 16:57:12 +0530

    fix: standard functions
    when standard function is using another one then it won't work in
    some cases as dependent function will be part of source code.
    This is fixed by using nested function.

--- test/scenarios/standard_functions/data.ts
+++ test/scenarios/standard_functions/data.ts
@@ -1,16 +1,68 @@
 import { Scenario } from '../../types';
 
+const input = {
+  arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  obj: {
+    foo: 1,
+    bar: 2,
+    baz: 3,
+    quux: 4,
+  },
+};
+
 export const data: Scenario[] = [
   {
-    input: {
-      arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      obj: {
-        foo: 1,
-        bar: 2,
-        baz: 3,
-        quux: 4,
-      },
-    },
+    template: '.arr.avg()',
+    input,
+    output: 5.5,
+  },
+  {
+    template: '.arr.first()',
+    input,
+    output: 1,
+  },
+  {
+    template: '.arr.index(0)',
+    input,
+    output: 1,
+  },
+  {
+    template: '.arr.index(9)',
+    input,
+    output: 10,
+  },
+  {
+    template: '.arr.last()',
+    input,
+    output: 10,
+  },
+  {
+    template: '.arr.length()',
+    input,
+    output: 10,
+  },
+  {
+    template: '.arr.min()',
+    input,
+    output: 1,
+  },
+  {
+    template: '.arr.max()',
+    input,
+    output: 10,
+  },
+  {
+    template: '.arr.stddev()',
+    input,
+    output: 2.8722813232690143,
+  },
+  {
+    template: '.arr.sum()',
+    input,
+    output: 55,
+  },
+  {
+    input,
     output: {
       sum: 55,
       sum2: 55,
